1132f39b20f1a73c0133a2690b28cc78e3dfdf84
Maximilian <61150071+MaxPW777@users.noreply.github.com>
2023-11-09 16:39:23 +0100

    changed file structure

---
+++ client/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'src/main.tsx', // Set the entry point to "client/src/main.tsx"
+      },
+    },
+  },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+  }
+
+})
