b7601bc7fcae34020de6a7b776f356c86b449867
Maximilian <61150071+MaxPW777@users.noreply.github.com>
2023-11-10 14:47:27 +0100

    [+] created basic search structure

--- client/src/components/Searchbar.tsx
+++ client/src/components/Searchbar.tsx
@@ -1,8 +1,33 @@
 import "../styles/Searchbar.css"
 
-function Searchbar() {
+interface SearchbarProps {
+  cards?: any
+}
+
+function Searchbar({ cards }: SearchbarProps) {
   return (
-    <div className='Searchbar'></div>
+    <div className='Searchbar'>
+      <div className="SearchForm">
+        <input type="text" />
+        <button>Search</button>
+      </div>
+      <div className="SearchResults">
+        {cards ? cards.map((card: any) => {
+          return (
+            <div className="SearchItem">
+              <img src={""} alt={""} />
+              <div className="CardInfo">
+                <h3>{card.name}</h3>
+              </div>
+              <button>Add to Deck</button>
+            </div>
+          )
+        })
+          :
+          <p> flop </p>
+        }
+      </div>
+    </div>
   )
 }
 
