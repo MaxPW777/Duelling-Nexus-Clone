cc1b662228e991007d07ddc6da12e72eb7425ca8
Maximilian <61150071+MaxPW777@users.noreply.github.com>
2023-11-10 15:36:09 +0100

    [+] added card interface

--- client/src/components/SearchItem.tsx
+++ client/src/components/SearchItem.tsx
@@ -1,17 +1,18 @@
+import { Carte } from "../interfaces/carte"
 
 interface SearchItemProps {
-  card?: any
+  card?: Carte
 }
 
 function SearchItem({ card }: SearchItemProps) {
   return (
     <div className="SearchItem">
-              <img src={""} alt={""} />
-              <div className="CardInfo">
-                <h3>{card}</h3>
-              </div>
-              <button>Add to Deck</button>
-            </div>
+      <img src={""} alt={""} />
+      <div className="CardInfo">
+        <h3>{card?.Name}</h3>
+      </div>
+      <button>Add to Deck</button>
+    </div>
   )
 }
 
---
+++ client/src/interfaces/carte.ts
@@ -0,0 +1,10 @@
+export interface Carte {
+  ID: number;
+  Name: string;
+  Type: string;
+  Atk: number;
+  Def: number;
+  image: string;
+  Description: string;
+  Rarete: string;
+}
