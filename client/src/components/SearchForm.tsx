43664bd314e0ab1f0cd6ea57b33e19210cb4a34d
Maximilian <61150071+MaxPW777@users.noreply.github.com>
2023-11-10 17:34:09 +0100

    [~] opti search form

--- client/src/components/SearchForm.tsx
+++ client/src/components/SearchForm.tsx
@@ -1,20 +1,23 @@
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 
 function SearchForm() {
-  const inputRef = useRef<HTMLInputElement>(null);
   const [search, setSearch] = useState('');
 
   function handleSearch(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault(); // Prevent the default form submission behavior
-    setSearch(inputRef.current?.value || '');
+    //TODO: Add search logic
+  }
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setSearch(event.target.value);
   }
 
   return (
-      <form onSubmit={handleSearch}>
-        <input ref={inputRef} type="text" />
-        <button type="submit">Search</button>
-      </form>
-    
+    <form onSubmit={handleSearch}>
+      <input onChange={handleChange} type="text" />
+      <button type="submit">Search</button>
+    </form>
+
   );
 }
 
