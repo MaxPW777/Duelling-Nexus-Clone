c2f70db50097017aeb7474db52b617dff0ddf3ba
Maximilian <mpinderwhite@gmail.com>
2023-11-12 16:49:02 +0100

    [~] update searchItem and package.json

--- client/src/components/SearchItem.tsx
+++ client/src/components/SearchItem.tsx
@@ -1,19 +1,19 @@
 import Carte from "../interfaces/carte"
 
 interface SearchItemProps {
-  card?: Carte
+  card: Carte
 }
 
 function SearchItem({ card }: SearchItemProps) {
 
   return (
-    <button  className="SearchItem">
-      
-      <img src={card?.image} alt={card?.Nom + "image"} />
+    <button className="SearchItem">
+
+      <img className="SearchCardImage" src={"public/yugioh-monstre.png"} alt={card.Nom + "image"} />
       <div className="CardInfo">
-        <h3>{card?.Nom}</h3>
-        <p>{card?.Atk}</p>
-        <p>{card?.Def}</p>
+        <h3>{card.Nom}</h3>
+        <p>{card.Type}</p>
+        <div>{card.Atk +" / "+ card.Def}</div>
       </div>
     </button>
   )
