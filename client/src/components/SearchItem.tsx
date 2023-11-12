0568d751d0dfcbcac000af4f7ae7d2719efc5aab
Maximilian <mpinderwhite@gmail.com>
2023-11-12 17:22:01 +0100

    [+] changed css to match better

--- client/src/components/SearchItem.tsx
+++ client/src/components/SearchItem.tsx
@@ -9,11 +9,11 @@ function SearchItem({ card }: SearchItemProps) {
   return (
     <button className="SearchItem">
 
-      <img className="SearchCardImage" src={"public/yugioh-monstre.png"} alt={card.Nom + "image"} />
-      <div className="CardInfo">
+      <img className="SearchCardImage" src={"public/yugioh-piege.png"} alt={card.Nom + "image"} />
+      <div className="CardStats">
         <h3>{card.Nom}</h3>
         <p>{card.Type}</p>
-        <div>{card.Atk +" / "+ card.Def}</div>
+        <div>{card.Atk && card.Atk +" / "+ card.Def}</div>
       </div>
     </button>
   )
