4a8d9596c85e7b6058b2dc9fa5eedfc313d42f47
Maximilian <61150071+MaxPW777@users.noreply.github.com>
2023-11-16 16:21:37 +0100

    [~] changed header css

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,7 +1,26 @@
+import { IoLogInOutline } from "react-icons/io5";
+import "../styles/Header.css";
 
 function Header() {
+  const isLoggedIn = true
   return (
-    <header></header>
+    <header>
+      <div className="loginTitleZone">
+        {
+          isLoggedIn &&
+          <a href="/login">
+            <IoLogInOutline />
+          </a>
+        }
+        <h1>PRYugiohSON</h1>
+      </div>
+      <p>Editing Deck:</p>
+      <div>
+        <button>SAVE</button>
+        <button>CLEAR</button>
+        <button>PARTAGER</button>
+      </div>
+    </header>
   )
 }
 
