efd01849784c7443543eb4de96a091389c9db1d0
Maximilian <61150071+MaxPW777@users.noreply.github.com>
2023-11-23 16:45:48 +0100

    [+ ~] fixed merge errors + got new decks

--- client/src/pages/deckedit/components/Decklist.tsx
+++ client/src/pages/deckedit/components/Decklist.tsx
@@ -1,13 +1,40 @@
 import React from 'react'
 import CryptoJS from 'crypto-js';
+import React, { useEffect, useState } from 'react'
+import { set } from 'react-hook-form'
+
+type Deck = {
+  DeckID: number,
+  DeckNom: string,
+  DeckDescription: string
+}
+
 function Decklist() {
-  const idFromLocalStorage : string = localStorage.getItem('userId') || '';
+  const idFromLocalStorage: string = localStorage.getItem('userId') || '';
   // Déchiffrez l'ID depuis le localStorage
   const decryptedUserId = CryptoJS.AES.decrypt(idFromLocalStorage, 'your-secret-key').toString(CryptoJS.enc.Utf8);
   console.log('Déchiffré :', decryptedUserId);
+  
+  const [deck, setDeck] = useState<Deck[]>([])
+  useEffect(() => {
+    fetch('http://localhost:3001/deck')
+      .then(res => res.json())
+      .then(data => setDeck(data))
+  }, []
+  )
   return (
     <div className='deckList'>
-      
+      {
+        deck && deck.map((deck) => {
+          return (
+            <div className='deck' key={deck.DeckID}>
+              <h3>{deck.DeckNom}</h3>
+              <p>{deck.DeckDescription}</p>
+            </div>
+          )
+        })
+      }
+
     </div>
   )
 }
