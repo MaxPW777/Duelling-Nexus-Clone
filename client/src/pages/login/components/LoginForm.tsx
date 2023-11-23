57bad702f412a7602256432b4c98532bd453fe73
Maximilian <61150071+MaxPW777@users.noreply.github.com>
2023-11-23 14:57:33 +0100

    [+] react hook form

--- client/src/pages/login/components/LoginForm.tsx
+++ client/src/pages/login/components/LoginForm.tsx
@@ -1,13 +1,28 @@
+import { useForm, SubmitHandler } from "react-hook-form"
+
+type FormValues = {
+    username: string
+    password: string
+}
+
 function LoginForm() {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm<FormValues>()
+    const onSubmit: SubmitHandler<FormValues> = (data) => console.log(data)
+
     return (
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)} className="loginForm">
             <div className='formText'>
                 <label htmlFor="username">Username</label>
-                <input type="text" id="username" />
+                <input {...register("username", { required: true })} type="text" id="username" />
             </div>
             <div className='formText'>
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password" />
+                <input {...register("password", { required: true })} type="password" id="password" />
             </div>
             <button type="submit">Login</button>
         </form>
